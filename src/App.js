d8b195e8bab7ec1c4dc27107d5a56dddb2b75cb2
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-26 18:19:26 -0500

    layout.

--- src/App.js
+++ src/App.js
@@ -27,13 +27,13 @@ class App extends Component {
     return (
       <div className="App">
         <div className="pure-g">
-          <div className="pure-u-1-2">
+          <div className="pure-u-1 pure-u-md-1-2">
             <div className={`l-box background-${v}-a`}>
               <i className={`logo fas fa-cube foreground-${v}-a`} />
               <h1 className={`App-title foreground-${v}-a`}>BITBOX</h1>
             </div>
           </div>
-          <div className="pure-u-1-2">
+          <div className="pure-u-1 pure-u-md-1-2">
             <div className={`l-box background-${v}-b`}>
               <i className={`logo fas fa-cube foreground-${v}-b`} />
               <h1 className={`App-title foreground-${v}-b`}>BITBOX</h1>
@@ -41,13 +41,13 @@ class App extends Component {
           </div>
         </div>
         <div className="pure-g">
-          <div className="pure-u-1-2">
+          <div className="pure-u-1 pure-u-md-1-2">
             <div className={`l-box background-${v}-c`}>
               <i className={`logo fas fa-cube foreground-${v}-c`} />
               <h1 className={`App-title foreground-${v}-c`}>BITBOX</h1>
             </div>
           </div>
-          <div className="pure-u-1-2">
+          <div className="pure-u-1 pure-u-md-1-2">
             <div className={`l-box background-${v}-d`}>
               <i className={`logo fas fa-cube foreground-${v}-d`} />
               <h1 className={`App-title foreground-${v}-d`}>BITBOX</h1>
